40b13021d36d58334a6451f14b54d006a14e48b2
Alexander König <Alexander.Koenig@eurac.edu>
2017-05-23 17:21:25 +0200

    commented selenium jar config back in

--- protractor.conf.js
+++ protractor.conf.js
@@ -57,7 +57,7 @@ if (process.env.TRAVIS) {
   }
   ];
 } else {
-//  config.seleniumServerJar = './node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar';
+  config.seleniumServerJar = './node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar';
 }
 
 exports.config = config;
